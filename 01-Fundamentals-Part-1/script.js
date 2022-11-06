4604219aed4274c6fb00c45047f33dfe617cd763
Luiz Andrade <109535506+LuizAndradeFilho@users.noreply.github.com>
2022-11-06 08:06:04 -0500

    up

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,13 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+//Camel Uppercase (Variaveis com mais de uma palavra, a partir da segunda vai com maiuscula na primeira letra)
+let firstName = "Jonas";
+
+// Variaveis que comecam com NUMEROS nao sao permitidas em JS
+// let 3Years = 3;
+
+console.log(firstName);

f0cb8926557dc79167038ceb4b5437c93e997d2d
Luiz Andrade <109535506+LuizAndradeFilho@users.noreply.github.com>
2022-11-12 13:17:01 -0500

    Update script.js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -268,3 +268,6 @@ console.log(birthYear);
 const inputYear = "1991";
 console.log(Number(inputYear));
 console.log(Number(inputYear) + 18);
+
+console.log(Number("Jonas"));
+console.log(typeof NaN);

a42a8a7616c0b467b2f05463b9e37aa6814c663b
Luiz Andrade <109535506+LuizAndradeFilho@users.noreply.github.com>
2022-11-17 11:23:32 -0500

    Update script.js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -265,9 +265,18 @@ console.log(birthYear);
 
 //TYPE CONVERSION AND COERCION
 
+// type conversion
 const inputYear = "1991";
 console.log(Number(inputYear));
 console.log(Number(inputYear) + 18);
 
 console.log(Number("Jonas"));
 console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+//type coercion = JS vai converter um numero aultomaticamente em string nese caso gracas ao +
+console.log("I am " + 23 + " years old");
+console.log("23" - "10" - 3);
+console.log("23" + "10" + 3);
+console.log("23" / "2");

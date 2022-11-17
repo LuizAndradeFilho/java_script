5c4349cb78ef954b44db649179169ee6b2dec240
Luiz Andrade <109535506+LuizAndradeFilho@users.noreply.github.com>
2022-11-17 13:49:35 -0500

    up

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -266,17 +266,50 @@ console.log(birthYear);
 //TYPE CONVERSION AND COERCION
 
 // type conversion
-const inputYear = "1991";
-console.log(Number(inputYear));
-console.log(Number(inputYear) + 18);
+// const inputYear = "1991";
+// console.log(Number(inputYear));
+// console.log(Number(inputYear) + 18);
 
-console.log(Number("Jonas"));
-console.log(typeof NaN);
+// console.log(Number("Jonas"));
+// console.log(typeof NaN);
 
-console.log(String(23), 23);
+// console.log(String(23), 23);
 
-//type coercion = JS vai converter um numero aultomaticamente em string nese caso gracas ao +
-console.log("I am " + 23 + " years old");
-console.log("23" - "10" - 3);
-console.log("23" + "10" + 3);
-console.log("23" / "2");
+// //type coercion = JS vai converter um numero aultomaticamente em string nese caso gracas ao +
+// console.log("I am " + 23 + " years old");
+// console.log("23" - "10" - 3);
+// console.log("23" + "10" + 3);
+// console.log("23" / "2");
+
+// let n = "1" + 1;
+// n = n - 1;
+// console.log(n);
+
+// // ----------------------------------------------------------------------------------------------------------------------------------------------------
+// TRUTHY and FALSY values
+// 5 Falsy Valuer: 0, '', undefined, null, NaN
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean("Jonas"));
+// console.log(Boolean({}));
+// console.log(Boolean(""));
+
+// const money = 0;
+// if (money) {
+//   console.log(`Don't spend it all ;)`);
+// } else {
+//   console.log(`You should get a Job!`);
+// }
+
+// let height;
+// if (height) {
+//   console.log("Yay! Height is defined!");
+// } else {
+//   console.log("Height is UNDEFINED");
+// }
+
+// EQUALITY OPERATORS: == VS. ===
+
+const age = 18;
+if (age === 18) console.log(`You just became an adult!`);

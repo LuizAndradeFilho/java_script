ceb982793ecb0d328a3571fab41d9be047044aa0
Luiz Andrade <109535506+LuizAndradeFilho@users.noreply.github.com>
2023-01-11 14:42:56 -0500

    Update script.js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -395,26 +395,55 @@ console.log(averageKoalas);
 
 // ----------------------------------------------------------------------------------------------------------------------------------------------------
 
-const day = "saturday";
+// const day = "friday";
 
-switch (day) {
-  case "monday": // day === 'monday'
-    console.log("Plan course structure");
-    console.log("Go to coding meetup");
-    break;
-  case " tuesday":
-    console.log("Prepare theory videos");
-    break;
-  case "wednesday":
-  case " thursday":
-    console.log("Write code examples");
-  case "friday":
-    console.log("Record Videos");
-    break;
-  case "saturday":
-  case "sunday":
-    console.log("Enjour the weekend");
-    break;
-  default:
-    console.log("Not a valid day!");
+// switch (day) {
+//   case "monday": // day === 'monday'
+//     console.log("Plan course structure");
+//     console.log("Go to coding meetup");
+//     break; // importamnte para parar o codigo
+//   case "tuesday":
+//     console.log("Prepare theory videos");
+//     break;
+//   case "wednesday":
+//   case "thursday":
+//     console.log("Write code examples");
+//     break;
+//   case "friday":
+//     console.log("Record Videos");
+//     break;
+//   case "saturday":
+//   case "sunday":
+//     console.log("Enjour the weekend");
+//     break;
+//   default:
+//     console.log("Not a valid day!");
+// }
+
+// if (day === "monday") {
+//   console.log("Plan course structure");
+//   console.log("Go to coding meetup");
+// } else if (day === "tuesday") {
+//   console.log("Prepare theory videos");
+// } else if (day === "wednesday" || day === "thursday") {
+//   console.log("Write code examples");
+// } else if (day === "friday") {
+//   console.log("Record Videos");
+// } else if (day === "saturday" || day === "sunday") {
+//   console.log("Enjoy your the weekend");
+// } else {
+//   console.log("Not a valid day!");
+// }
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------
+
+// EXPRESSIONS = Produzem um valor
+// exemplos
+// 3 + 4
+// 1991
+// ture && false && !false
+
+// DECLARACAOES (STATEMENTS) = Nao produzem valores por si mesmas
+if (23 > 10) {
+  const str = "23 is bigger";
 }

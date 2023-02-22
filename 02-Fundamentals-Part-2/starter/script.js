661088c663f91f1427a21713e0c560a9c61bbc66
Luiz Andrade <109535506+LuizAndradeFilho@users.noreply.github.com>
2023-02-22 15:42:08 -0500

    Update script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -147,5 +147,5 @@ HINT: To check if number A is at least double number B, check for A >= 2 * B. Ap
 GOOD LUCK 😀
 */
 
-
-function = calcAverage => ()
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(3, 4, 5));

248cfed331ed7176b6285cefd5c380fd3bf5a8a1
Luiz Andrade <109535506+LuizAndradeFilho@users.noreply.github.com>
2023-02-22 15:57:00 -0500

    Update script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -149,3 +149,7 @@ GOOD LUCK 😀
 
 const calcAverage = (a, b, c) => (a + b + c) / 3;
 console.log(calcAverage(3, 4, 5));
+
+//Test 1
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
